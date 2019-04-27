41d4eba629a6a8857af8301b900e4bc6664dbcef
gvs0709 <gvillaress@gmail.com>
2019-04-26 22:27:15 -0300

    Esqueleto inicial de nopen.c

---
+++ Trabalho-3/nopen.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+
+extern int isopen (int fd);
+
+int main (void){
+    int nopen, fd;
+
+    for (nopen = fd = 0; fd < getdtablesize(); fd++){
+        if (isopen (fd)){
+            nopen++;
+        }
+    }
+
+    printf ("Existem %d descritores abertos\n", nopen);
+    return (0);
+}
+
+int isopen(int fd){
+    //code
+    return 0;
+}

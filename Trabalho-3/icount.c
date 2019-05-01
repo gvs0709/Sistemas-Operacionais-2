05167f8a70ee5d687947cc0e8bef6a1bbee102b4
gvs0709 <gvillaress@gmail.com>
2019-04-30 23:32:55 -0300

    comentado um print em icount.c

--- Trabalho-3/icount.c
+++ Trabalho-3/icount.c
@@ -7,15 +7,16 @@
 #include <unistd.h>
 
 int walk_dir(const char *path, void (*func) (const char *)){
-
 	DIR *dirp;
 	struct dirent *dp;
 	char *p, *full_path;
 	int len;
 
 	/* abre o diretório */
-	if ((dirp = opendir (path)) == NULL)
-		return (-1);
+	if ((dirp = opendir (path)) == NULL) {
+        return (-1);
+    }
+
 	len = strlen (path);
 
 	/* aloca uma área na qual,garantidamente, o caminho caberá */
@@ -32,8 +33,10 @@ int walk_dir(const char *path, void (*func) (const char *)){
 	/* deixa “p” no lugar certo! */
 	while ((dp = readdir (dirp)) != NULL){
 		/* ignora as entradas “.” e “..” */
-		if (strcmp (dp->d_name, ".") == 0 || strcmp (dp->d_name, "..") == 0)
-			continue;
+		if (strcmp (dp->d_name, ".") == 0 || strcmp (dp->d_name, "..") == 0) {
+            continue;
+        }
+
 		strcpy (p, dp->d_name);
 
 		/* “full_path” armazena o caminho */(*func) (full_path);
@@ -43,13 +46,11 @@ int walk_dir(const char *path, void (*func) (const char *)){
 	closedir (dirp);
 
 	return (0);
-}
-/* end walk_dir */
+}/* end walk_dir */
 
 int file_type, contador = 0;
 
 void conta(const char *path){
-
 	struct stat buf;
 
 	stat(path, &buf);
@@ -86,32 +87,33 @@ void conta(const char *path){
 }
 
 int main(int argc, char **argv){
-    int opt, mod_pos = -1;
+    int opt, mod_pos = -1, num_mods = 0;
     char *path;
 
-    int num_mods = 0;
-
     while((opt = getopt(argc, argv, "rdlbc")) != -1){
-
     	switch(opt){
     		case 'r':
     			file_type = 0;
-    			break; 
+    			break;
+
     		case 'd':
     			file_type = 1;
     			break;
+
     		case 'l':
     			file_type = 2;
     			break;
+
     		case 'b':
     			file_type = 3;
     			break;
+
     		case 'c':
     			file_type = 4;
     			break;
+
     		default:
     			file_type = 0;
-    			break;
     	}
 
     	num_mods++;
@@ -124,15 +126,18 @@ int main(int argc, char **argv){
     	}
     }
 
-    printf("mod_pos: %d\n", mod_pos);
+    //printf("mod_pos: %d\n", mod_pos);
 
     if(argc < 2 || optind >= argc){
     	path = ".";
     	walk_dir(path, (void *) conta);
     	printf("%d\n", contador);
-    }else{
+    }
+
+    else{
     	for(int i = 1; i < argc; i++){
     		contador = 0;
+
     		if(i == mod_pos){
     			continue;
     		}

54f222655ea85d46f29641927f7347be8b7afa75
Bruno <bruno.meurer11@gmail.com>
2019-05-24 22:38:53 -0300

    Adicionando mais infos ao comentário do script  7

--- Trabalho-4/script-7.sh
+++ Trabalho-4/script-7.sh
@@ -2,7 +2,8 @@
 
 cat /etc/passwd | while read line #Lê cada linha do arquivo
 do
-	#Para cada linha, utiliza o awk para remover os ":" separadores 
-	#e utiliza o printf para mostrar a saída na formatação desejada
+	#Para cada linha, utiliza o awk para remover os ":" separadores, 
+	#utiliza o printf para mostrar a saída na formatação desejada e 
+	#remove "," do final da linha
 	echo "$line" | awk -F ':' '{printf "%s\t%s\n", $1, $5}' | tr ',' ' '
 done

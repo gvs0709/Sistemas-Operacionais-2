0d5f62d6e1dbf5616416ac39fd613936caf1d296
bmeurer1 <bruno.meurer11@gmail.com>
2019-05-26 02:27:07 -0300

    Incluindo um else no script 5 para ele retornar alguma coisa caso a substring não esteja contida

--- Trabalho-4/script-5.sh
+++ Trabalho-4/script-5.sh
@@ -7,7 +7,8 @@ fi
 
 if [[ "$2" == *"$1"* ]]; then #Utiliza wildcard * para expandir a substring para ambos os lados
     echo "$1 está contida em $2" #Se der um match, a substring existe na string 
-    
+else
+	echo "$1 não está contida em $2"
 fi
 
 exit 0

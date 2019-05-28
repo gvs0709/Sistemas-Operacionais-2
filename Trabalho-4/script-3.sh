7666625fc0355e38d3d5911d9e2645ff74acd932
bmeurer1 <bruno.meurer11@gmail.com>
2019-05-28 08:31:20 -0300

    Melhora do tratamento de casos inválidos no script 3

--- Trabalho-4/script-3.sh
+++ Trabalho-4/script-3.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+#Exit if any of the parameters is empty
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+    echo "Invalid comparison"
+    exit 0
+fi
+
+if [[ "$1" == [A-Za-z] ]] || [[ "$2" == [A-Za-z] ]] ; then
+    echo "Invalid comparison"
+    exit 0
+fi
+
 # Test if value 1 is greater, smaller or equal to value 2
-if [ "$1" -gt "$2" ]; then
+if [[ "$1" -gt "$2" ]]; then
 	echo "$1 is greater than $2"
 
-elif [ "$1" -lt "$2" ]; then
+elif [[ "$1" -lt "$2" ]]; then
 	echo "$1 is smaller than $2"
 
-elif [ "$1" -eq "$2" ]; then
+elif [[ "$1" -eq "$2" ]]; then
 	echo "$1 and $2 are the same"
 
 else

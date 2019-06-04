09198d89eb9fa752a8c0021e943bf8dba125fbe9
bmeurer1 <bruno.meurer11@gmail.com>
2019-06-04 08:52:11 -0300

    Descomentando o else do srcipt 5

--- Trabalho-4/script-5.sh
+++ Trabalho-4/script-5.sh
@@ -8,8 +8,8 @@ fi
 if [[ "$2" == *"$1"* ]]; then #Utiliza wildcard * para expandir a substring para ambos os lados
     echo "$1 está contida em $2" #Se der um match, a substring existe na string 
     
-#else
-#	echo "$1 não está contida em $2"
+else
+	echo "$1 não está contida em $2"
 
 fi
 

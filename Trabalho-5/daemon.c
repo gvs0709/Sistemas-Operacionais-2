72ba0d551949c1cb1b2e5f331b86533e34eba3f0
gvs0709 <gvillaress@gmail.com>
2019-06-20 01:42:43 -0300

    temporização e signal handler implementados assim como o listener dos sinais SIGTERM e SIGINT

--- Trabalho-5/daemon.c
+++ Trabalho-5/daemon.c
@@ -4,57 +4,72 @@
 #include <signal.h>
 #include <unistd.h>
 
-//int sair = 0;
+int pid;
 
 void signal_handler(int sig){
-    system("echo Encerrando daemon... >> daemon_log.txt");
-    //sair = 1;
+    //printf("%d Entrando no signal handler\n", pid);
+
+    //Se for o filho, atualiza o log
+    if(pid == 0){
+        system("ps aux | grep \"Z\" | grep -v \"grep\" > daemon_log.txt");
+    }
+    
+    else{
+        //Se for o pai e tiver recebido um SIGTERM, matar o filho e finalizar
+        if(sig == SIGTERM){
+            system("echo \"\nEncerrando daemon...\" >> daemon_log.txt");
+            kill(pid, 9);
+            exit(0);
+        }
+        
+        /*else{
+            fprintf(stdout, "Only SIGKILL and SIGTERM can end this daemon\n");
+        }*/
+    }
 }
 
 int main(int argc, char *argv[]){
-    FILE *log_file;
     int segundos = 2; // Tempo padrão de verificação
-    //clock_t parcial1, parcial2;
-    //struct sigaction act;
     
     if(argc > 1){
         segundos = atoi(argv[1]); // Se foi passado um tempo de verificação, troca o padrão pelo tempo passado
-        //printf("segundos = %d\n", segundos);
+        //printf("Segundos = %d\n", segundos);
     }
 
-    /*else{
-        printf("Nenhum argumento passado. Utilizando valor padrão (2s)\n");
-    }*/
+    pid = fork();
 
-    /*act.sa_handler = signal_handler;
-    sigfillset(&act.sa_mask);
-    sigdelset(&act.sa_mask, SIGINT);
-    sigaction(SIGINT, &act, NULL);*/
-    
-    log_file = fopen("daemon_log.txt", "w");
-    //parcial1 = clock(); // Começa a medir o tempo
-    //parcial2 = parcial1; // Parciais começam iguais
+    sigset_t mask;
+    sigfillset(&mask);//Cria máscara ignorando todos os sinais inicialmente
+    sigdelset(&mask, SIGTERM);//Faz alteração para máscara não ignorar o SIGTERM
+    sigdelset(&mask, SIGINT);//Faz alteração para máscara não ignorar o SIGTNT
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    struct sigaction act;
+
+    //Inicializa infos da sigaction
+    act.sa_handler = signal_handler;
+    act.sa_mask = mask;
+    act.sa_flags = SA_SIGINFO;
+
+    //Seta os listerners dos signals SIGTERM e SIGINT
+    sigaction(SIGTERM, &act, NULL);
+    sigaction(SIGINT, &act, NULL);
 
     while(1){
-        //if((parcial2 * 1000. / CLOCKS_PER_SEC) - (parcial1 * 1000. / CLOCKS_PER_SEC) >= segundos){
-        if(fork() == 0){
-            system("ps aux | grep \"Z\" >> daemon_log.txt && echo \"====================================================================\" >> daemon_log.txt");
-
-            //parcial1 = clock(); // Nova parcial 1 para começar a contar até 'segundos' novamente
-            exit(0);
+        if(pid == 0){
+            //printf("Aguardando sinal do pai\n");
+            pause();
         }
-
-        //parcial2 = clock(); // Atualiza parcial 2 até diferença com parcial 1 ser >= a 'segundos'
-        sleep(segundos);
         
-        //fprintf(log_file, "%d\n", segundos);
+        else{
+            sleep(segundos);
+            //printf("Enviando sinal para o filho\n");
 
-        /*if(sair){
-            break;
-        }*/
+            //A cada intervalo de segundos, envia um sinal para o filho para
+            //avisar que tem que atualizar o log
+            kill(pid, 2);
+        }
     }
-    
-    fclose(log_file);
     
     return 0;
 }
